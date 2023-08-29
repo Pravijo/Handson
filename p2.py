7b6675a247f90004008c5e0da87727a1258174fe
Pravijo <mpravalika89@gmail.com>
2023-08-29 00:45:50 -0400

    Practice Programs

--- p2.py
+++ p2.py
@@ -1,13 +1,25 @@
+"""
 a = input("enter number ")
 b = input("enter number ")
 c = input("enter number ")
 d = input("enter number ")
 e = input("enter number ")
 f = input("enter number ")
-list = [a,b,c,d,e,f]
+g = input("enter number")
+list = [a,b,c,d,e,f,g]
+print(list)
 l1 = list[:3]
 print(l1)
 l2 = list[-3:]
 print(l2)
 l3 = l1 + l2
-print(l3)
+print(l3)"""
+
+l1 = [1,2,3,4,44,5,55,66,77,85,95]
+last3 = []
+for i in range(-1, -4, -1):
+    last3.append(l1[i])
+print(last3)
+
+#listR = list(reversed(last3))  #reversed list in listR
+#print(listR)
--- p3.py
+++ p3.py
@@ -1,6 +1,16 @@
+"""
 l1 = []
 a = int(input("enter your number "))
 for i in range(0,a):
     if i % 5 == 0:
         l1.append(i)
-print(l1)
+print(l1)  """
+
+
+l = [10,23,34,45,56,67,88,85,70]
+l2 = []
+for i in l:
+    if i % 5 == 0:
+        l2.append(i)
+print(l2)
+
